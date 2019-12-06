0912b7eba9db00cb6e886626f72ded6817d7ef2e
Kosuke Kito <blueberryjam09@gmail.com>
2019-12-06 17:24:18 +0900

    Add sample code

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,10 @@ class MyApp extends StatelessWidget {
       title: 'Flutter Demo',
       theme: ThemeData(
         primarySwatch: Colors.blue,
-        tabBarTheme: TabBarTheme(labelColor: Colors.black),
       ),
-      home: const MyHomePage(),
+      home: const StickyTabsPage(),
+//      home: const NestedScrollViewSamplePage(),
+//      home: const StickyTabsPage(),
     );
   }
 }
@@ -22,13 +23,15 @@ const _tabs = [
   'Cherry',
 ];
 
-class MyHomePage extends StatelessWidget {
-  const MyHomePage();
+const String _appBarTitle = 'Fruits';
+
+class StickyTabsPage extends StatelessWidget {
+  const StickyTabsPage();
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Fruits'), elevation: 0.0),
+      appBar: AppBar(title: const Text(_appBarTitle), elevation: 0.0),
       body: DefaultTabController(
         length: _tabs.length, // This is the number of tabs.
         child: NestedScrollView(
@@ -101,11 +104,80 @@ class _StickyTabBarDelegate extends SliverPersistentHeaderDelegate {
     double shrinkOffset,
     bool overlapsContent,
   ) {
-    return Container(color: Colors.white, child: tabBar);
+    return Container(color: Colors.blue, child: tabBar);
   }
 
   @override
   bool shouldRebuild(_StickyTabBarDelegate oldDelegate) {
     return tabBar != oldDelegate.tabBar;
+  }
+}
+
+class NestedScrollViewSamplePage extends StatelessWidget {
+  const NestedScrollViewSamplePage();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: DefaultTabController(
+        length: _tabs.length,
+        child: NestedScrollView(
+          headerSliverBuilder: (BuildContext context, bool innerBoxIsScrolled) {
+            return <Widget>[
+              SliverOverlapAbsorber(
+                handle:
+                    NestedScrollView.sliverOverlapAbsorberHandleFor(context),
+                child: SliverAppBar(
+                  title: const Text(_appBarTitle),
+                  pinned: true,
+                  expandedHeight: 150.0,
+                  forceElevated: innerBoxIsScrolled,
+                  bottom: TabBar(
+                    tabs: _tabs.map((String name) => Tab(text: name)).toList(),
+                  ),
+                ),
+              ),
+            ];
+          },
+          body: TabBarView(
+            children: _tabs.map((String name) {
+              return SafeArea(
+                top: false,
+                bottom: false,
+                child: Builder(
+                  builder: (BuildContext context) {
+                    return CustomScrollView(
+                      key: PageStorageKey<String>(name),
+                      slivers: <Widget>[
+                        SliverOverlapInjector(
+                          handle:
+                              NestedScrollView.sliverOverlapAbsorberHandleFor(
+                            context,
+                          ),
+                        ),
+                        SliverPadding(
+                          padding: const EdgeInsets.all(8.0),
+                          sliver: SliverFixedExtentList(
+                            itemExtent: 48.0,
+                            delegate: SliverChildBuilderDelegate(
+                              (BuildContext context, int index) {
+                                return ListTile(
+                                  title: Text('Item $index'),
+                                );
+                              },
+                              childCount: 30,
+                            ),
+                          ),
+                        ),
+                      ],
+                    );
+                  },
+                ),
+              );
+            }).toList(),
+          ),
+        ),
+      ),
+    );
   }
 }
